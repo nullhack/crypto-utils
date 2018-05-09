00db5dceed3e7ec8d15bdf83c224b4518fe73e3d
Eric Lopes <eric.oliveira.lopes@gmail.com>
2018-05-09 17:36:49 +0000

    create exchanges_api

--- bitcoin_listener.py
+++ bitcoin_listener.py
@@ -9,6 +9,7 @@ import random
 import sys
 import doctest
 from decimal import Decimal, Context, ROUND_HALF_EVEN
+from exchanges_api import DEFAULT_EXCHANGE, EXCHANGES
 
 class OrderBook(Thread):
     """This class handle read and write methods for bitcoin data.
@@ -30,110 +31,9 @@ class OrderBook(Thread):
         EXCHANGES (dict): Dict of available exchanges.
     """
     
-    DEFAULT_EXCHANGE = 'bitstamp'
-    EXCHANGES = {
-        'bitstamp':{
-            'url':'http://www.bitstamp.net/api/order_book/',
-            'params':None,
-            'best_shift':{'price':2, 'amount':8},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'mercado_bitcoin':{
-            'url':'https://www.mercadobitcoin.com.br/api/orderbook/',
-            'params':None,
-            'best_shift':{'price':5, 'amount':5},
-            'delay':300,
-            'pair':{'BTC','BRL'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'okcoin':{
-            'url':'https://www.okcoin.com/api/v1/depth.do',
-            'params':{'symbol':'btc_usd'},
-            'best_shift':{'price':2, 'amount':8},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'korbit':{
-            'url':'https://api.korbit.co.kr/v1/orderbook',
-            'params':None,
-            'best_shift':{'price':-2, 'amount':5},
-            'delay':180,
-            'pair':{'BTC','KRW'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'itbit':{
-            'url':'https://api.itbit.com/v1/markets/XBTUSD/order_book',
-            'params':None,
-            'best_shift':{'price':2, 'amount':4},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'bitfinex':{
-            'url':'https://api.bitfinex.com/v1/book/btcusd',
-            'params':None,
-            'best_shift':{'price':2, 'amount':4},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t['price']), float(t['amount'])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t['price']), float(t['amount'])] for t in d['bids']], reverse=True),
-        },
-        'hitbtc':{
-            'url':'https://api.hitbtc.com/api/1/public/BTCUSD/orderbook',
-            'params':None,
-            'best_shift':{'price':2, 'amount':2},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'poloniex':{#poloniex has much more pairs!, problem of decimals on price
-            'url':'https://poloniex.com/public',
-            'params':{'command':'returnOrderBook',
-                    'currencyPair':'USDT_BTC',
-                    'depth':'6000'},
-            'best_shift':{'price':2, 'amount':8},
-            'delay':120,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['bids']], reverse=True),
-        },
-        'kraken':{#kraken has much more pairs!
-            'url':'https://api.kraken.com/0/public/Depth',
-            'params':{'pair':'XBTUSD'},
-            'best_shift':{'price':2, 'amount':3},
-            'delay':180,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['result']['XXBTZUSD']['asks']]),
-            'bids':lambda d:sorted([[float(t[0]), float(t[1])] for t in d['result']['XXBTZUSD']['bids']], reverse=True),
-        },
-        'coindesk':{#NOT ORDERBOOK! ONLY LAST TICK
-            'url':'https://api.coindesk.com/v1/bpi/currentprice/btcusd',
-            'params':None,
-            'best_shift':{'price':4, 'amount':0},
-            'delay':120,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:sorted([[float(d['bpi']['USD']['rate_float']), 1.0]]),
-            'bids':lambda d:sorted([[float(d['bpi']['USD']['rate_float']), 1.0]]),
-        },
-        'test':{
-            'url':None,
-            'params':None,
-            'best_shift':{'price':2, 'amount':8},
-            'delay':2,
-            'pair':{'BTC','USD'},
-            'asks':lambda d:[[6563.59, 1.61],[6563.62, 1.5],[6571.79, 1.8],[6571.8, 0.01]],
-            'bids':lambda d:[[6559.9, 0.02],[6559.62, 0.02],[6559.61, 0.2],[6558.1, 0.01]],
-        },
-    }
+    DEFAULT_EXCHANGE = DEFAULT_EXCHANGE
+
+    EXCHANGES = EXCHANGES
     
     def __init__(self, exchange = DEFAULT_EXCHANGE, price_num_digits=8, amount_num_digits=3):
         super(OrderBook, self).__init__()
@@ -326,6 +226,7 @@ class OrderBook(Thread):
         self.exchange = exchange if exchange in self.EXCHANGES.keys() else self.DEFAULT_EXCHANGE
 
     def run(self):
+        """Set thread to run and fetch orderbook for current exchange."""
         self.keep_running = True
         delay = self.EXCHANGES[self.exchange]['delay']
         time.sleep(random.randint(1, delay))
@@ -339,9 +240,11 @@ class OrderBook(Thread):
         print('Finishing', self.exchange)
 
     def stop(self):
+        """Stop fetching orderbook."""
         self._keep_running = False
         
     def terminate(self):
+        """Stop fetching orderbook and save current status."""
         self.stop()
         self.flush_orderbook()
 
